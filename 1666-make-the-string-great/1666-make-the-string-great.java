f1dc94c79e1d35c646e6647bd3312a6b93f8946e
Raunak <87171612+Raunak827@users.noreply.github.com>
2026-09-02 21:59:47 +0530

    Time: 1 ms (99.95%), Space: 43.2 MB (95.91%) - LeetHub

---
+++ 1666-make-the-string-great/1666-make-the-string-great.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String makeGood(String s) {
+        StringBuilder res= new StringBuilder();
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(res.length() >0 && Math.abs(ch-res.charAt(res.length()-1))==32) res.deleteCharAt(res.length()-1);
+            else res.append(ch);
+        }
+        return res.toString();
+    }
+}
